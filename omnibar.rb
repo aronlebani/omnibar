db8314fe01c561114f02510c6bdb63562b2ef46a
Aron Lebani <aron@lebani.dev>
2024-12-23 13:39:59 +1100

    Format

--- omnibar.rb
+++ omnibar.rb
@@ -5,10 +5,10 @@
 require 'bundler/inline'
 
 gemfile do
-  source 'https://rubygems.org'
+	source 'https://rubygems.org'
 
-  gem 'open3'
-  gem 'optparse'
+	gem 'open3'
+	gem 'optparse'
 end
 
 require 'open3'
@@ -16,296 +16,292 @@ require 'optparse'
 require 'uri'
 
 class String
-  def lsplit(char)
-    first, *rest = split(char)
-    [first, rest.join(char)]
-  end
+	def lsplit(char)
+		first, *rest = split(char)
+		[first, rest.join(char)]
+	end
 
-  def rsplit(char)
-    *rest, last = split(char)
-    [rest.join(char), last]
-  end
+	def rsplit(char)
+		*rest, last = split(char)
+		[rest.join(char), last]
+	end
 end
 
 module SQLite
-  def query_sql(file, query)
-    Open3.popen3('sqlite3', '-init', '/dev/null', file) do |stdin, stdout|
-      stdin.puts query
-      stdin.close
-      stdout.readlines
-    end
-  end
+	def query_sql(file, query)
+		Open3.popen3('sqlite3', '-init', '/dev/null', file) do |stdin, stdout|
+			stdin.puts query
+			stdin.close
+			stdout.readlines
+		end
+	end
 end
 
 module Parser
-  def whitespace?(line)
-    /^$/.match(line)
-  end
+	def whitespace?(line) = /^$/.match(line)
 
-  def comment?(line)
-    /^#/.match(line)
-  end
+	def comment?(line) = /^#/.match(line)
 end
 
 class SearchItem
-  TYPES = [:bookmark, :history, :search_engine].freeze
+	TYPES = [:bookmark, :history, :search_engine].freeze
 
-  attr_reader :title, :url, :date, :type
+	attr_reader :title, :url, :date, :type
 
-  def initialize(title, url, date, type)
-    unless TYPES.include?(type)
-      raise ArguementError "title must be one of #{TYPES.join(' ')}"
-    end
+	def initialize(title, url, date, type)
+		unless TYPES.include?(type)
+			raise ArguementError "title must be one of #{TYPES.join(' ')}"
+		end
 
-    @title = title.strip
-    @url = url.strip
-    @date = date
-    @type = type
-  end
+		@title = title.strip
+		@url = url.strip
+		@date = date
+		@type = type
+	end
 
-  def to_launcher_s
-    case @type
-    when :bookmark
-      "B #{@title} <#{@url}>"
-    when :history
-      if @date
-        "H #{@title} <#{@url}> (#{@date})"
-      else
-        "H #{@title} <#{@url}>"
-      end
-    when :search_engine
-      "#{@title.strip}: "
-    end
-  end
+	def to_launcher_s
+		case @type
+		when :bookmark
+			"B #{@title} <#{@url}>"
+		when :history
+			if @date
+				"H #{@title} <#{@url}> (#{@date})"
+			else
+				"H #{@title} <#{@url}>"
+			end
+		when :search_engine
+			"#{@title.strip}: "
+		end
+	end
 
-  class << self
-    def parse(str, &block)
-      url = parse_url(str)
-      search_term = parse_search_term(str)
-      search_engine = parse_search_engine(str)
+	class << self
+		def parse(str, &block)
+			url = parse_url(str)
+			search_term = parse_search_term(str)
+			search_engine = parse_search_engine(str)
 
-      block.call(url, search_term, search_engine)
-    end
+			block.call(url, search_term, search_engine)
+		end
 
-    private
+		private
 
-    def parse_url(str)
-      return unless str[0] == 'H' || str[0] == 'B'
+		def parse_url(str)
+			return unless str[0] == 'H' || str[0] == 'B'
 
-      url, _ = /<(.*)>/.match(str).captures
-      url&.strip
-    end
+			url, _ = /<(.*)>/.match(str).captures
+			url&.strip
+		end
 
-    def parse_search_term(str)
-      return if str[0] == 'H' || str[0] == 'B'
+		def parse_search_term(str)
+			return if str[0] == 'H' || str[0] == 'B'
 
-      _, term = str.lsplit(':')
-      term&.strip
-    end
+			_, term = str.lsplit(':')
+			term&.strip
+		end
 
-    def parse_search_engine(str)
-      return if str[0] == 'H' || str[0] == 'B'
+		def parse_search_engine(str)
+			return if str[0] == 'H' || str[0] == 'B'
 
-      se, _ = str.lsplit(':')
-      se&.strip
-    end
-  end
+			se, _ = str.lsplit(':')
+			se&.strip
+		end
+	end
 end
 
 class Browser
-  extend Parser
-  extend SQLite
+	extend Parser
+	extend SQLite
 
-  class << self
-    def history = []
-    def bookmarks = []
-    def search_engines = []
+	class << self
+		def history = []
+		def bookmarks = []
+		def search_engines = []
 
-    def all(exclude_bookmarks: false, exclude_history: false)
-      bookmarks = subclasses.reduce([]) { |acc, b| acc + b.bookmarks }
-      history = subclasses.reduce([]) { |acc, b| acc + b.history }
-      search_engines = subclasses.reduce([]) { |acc, b| acc + b.search_engines }
+		def all(exclude_bookmarks: false, exclude_history: false)
+			bookmarks = subclasses.reduce([]) { |acc, b| acc + b.bookmarks }
+			history = subclasses.reduce([]) { |acc, b| acc + b.history }
+			search_engines = subclasses.reduce([]) { |acc, b| acc + b.search_engines }
 
-      items = search_engines
-      items += bookmarks unless exclude_bookmarks
-      items += history unless exclude_history
+			items = search_engines
+			items += bookmarks unless exclude_bookmarks
+			items += history unless exclude_history
 
-      items.compact.uniq(&:url)
-    end
-  end
+			items.compact.uniq(&:url)
+		end
+	end
 end
 
 class UserDefined < Browser
-  def self.history
-    hist_file = File.join(ENV['XDG_CONFIG_HOME'], 'history')
+	def self.history
+		hist_file = File.join(ENV['XDG_CONFIG_HOME'], 'history')
 
-    return [] unless File.exist?(hist_file)
+		return [] unless File.exist?(hist_file)
 
-    File.readlines(hist_file).map do |line|
-      next if whitespace?(line) || comment?(line)
+		File.readlines(hist_file).map do |line|
+			next if whitespace?(line) || comment?(line)
 
-      title, url = /([^<>]*)\s+<(.*)>/.match(line).captures
-      SearchItem.new(title, url, nil, :history)
-    end
-  end
+			title, url = /([^<>]*)\s+<(.*)>/.match(line).captures
+			SearchItem.new(title, url, nil, :history)
+		end
+	end
 
-  def self.bookmarks
-    bm_file = File.join(ENV['XDG_CONFIG_HOME'], 'bookmarks')
+	def self.bookmarks
+		bm_file = File.join(ENV['XDG_CONFIG_HOME'], 'bookmarks')
 
-    return [] unless File.exist?(bm_file)
+		return [] unless File.exist?(bm_file)
 
-    File.readlines(bm_file).map do |line|
-      next if whitespace?(line) || comment?(line)
+		File.readlines(bm_file).map do |line|
+			next if whitespace?(line) || comment?(line)
 
-      title, url = /([^<>]*)\s+<(.*)>/.match(line).captures
-      SearchItem.new(title, url, nil, :bookmark)
-    end
-  end
+			title, url = /([^<>]*)\s+<(.*)>/.match(line).captures
+			SearchItem.new(title, url, nil, :bookmark)
+		end
+	end
 
-  def self.search_engines
-    se_file = File.join(ENV['XDG_CONFIG_HOME'], 'search_engines')
+	def self.search_engines
+		se_file = File.join(ENV['XDG_CONFIG_HOME'], 'search_engines')
 
-    return [] unless File.exist?(se_file)
+		return [] unless File.exist?(se_file)
 
-    File.readlines(se_file).map do |line|
-      next if whitespace?(line) || comment?(line)
+		File.readlines(se_file).map do |line|
+			next if whitespace?(line) || comment?(line)
 
-      title, url = line.lsplit('=')
-      SearchItem.new(title, url, nil, :search_engine)
-    end
-  end
+			title, url = line.lsplit('=')
+			SearchItem.new(title, url, nil, :search_engine)
+		end
+	end
 end
 
 class Luakit < Browser
-  def self.history
-    hist_file = File.join(ENV['XDG_DATA_HOME'], 'luakit', 'history.db')
+	def self.history
+		hist_file = File.join(ENV['XDG_DATA_HOME'], 'luakit', 'history.db')
 
-    return [] unless File.exist?(hist_file)
+		return [] unless File.exist?(hist_file)
 
-    query = <<~SQL
-      SELECT title, uri, last_visit
-      FROM history
-      ORDER BY last_visit DESC;
-    SQL
+		query = <<~SQL
+			SELECT title, uri, last_visit
+			FROM history
+			ORDER BY last_visit DESC;
+		SQL
 
-    query_sql(hist_file, query).map do |line|
-      next if whitespace?(line) || comment?(line)
+		query_sql(hist_file, query).map do |line|
+			next if whitespace?(line) || comment?(line)
 
-      *title, url, date = line.split('|')
-      SearchItem.new(title.join('|'), url, Time.at(date.to_i).to_s, :history)
-    end
-  end
+			*title, url, date = line.split('|')
+			SearchItem.new(title.join('|'), url, Time.at(date.to_i).to_s, :history)
+		end
+	end
 end
 
 class Qutebrowser < Browser
-  def self.history
-    hist_file = File.join(ENV['XDG_DATA_HOME'], 'qutebrowser', 'history.sqlite')
+	def self.history
+		hist_file = File.join(ENV['XDG_DATA_HOME'], 'qutebrowser', 'history.sqlite')
 
-    return [] unless File.exist?(hist_file)
+		return [] unless File.exist?(hist_file)
 
-    query = <<~SQL
-      SELECT title, url, atime
-      FROM history
-      ORDER BY atime DESC;
-    SQL
+		query = <<~SQL
+			SELECT title, url, atime
+			FROM history
+			ORDER BY atime DESC;
+		SQL
 
-    query_sql(hist_file, query).map do |line|
-      next if whitespace?(line) || comment?(line)
+		query_sql(hist_file, query).map do |line|
+			next if whitespace?(line) || comment?(line)
 
-      *title, url, date = line.split('|')
-      SearchItem.new(title.join('|'), url, Time.at(date.to_i).to_s, :history)
-    end
-  end
+			*title, url, date = line.split('|')
+			SearchItem.new(title.join('|'), url, Time.at(date.to_i).to_s, :history)
+		end
+	end
 
-  def self.bookmarks
-    bm_file = File.join(ENV['XDG_CONFIG_HOME'], 'qutebrowser', 'quickmarks')
+	def self.bookmarks
+		bm_file = File.join(ENV['XDG_CONFIG_HOME'], 'qutebrowser', 'quickmarks')
 
-    return [] unless File.exist?(bm_file)
+		return [] unless File.exist?(bm_file)
 
-    File.readlines(bm_file).map do |line|
-      next if whitespace?(line) || comment?(line)
+		File.readlines(bm_file).map do |line|
+			next if whitespace?(line) || comment?(line)
 
-      title, url = line.split(' ')
-      SearchItem.new(title, url, nil, :bookmark)
-    end
-  end
+			title, url = line.split(' ')
+			SearchItem.new(title, url, nil, :bookmark)
+		end
+	end
 end
 
 LAUNCHER = ['dmenu', '-i', '-l', '10', '-p', 'Search:'].freeze
 SCHEMES = ['http://', 'https://', 'file://'].freeze
 
 def extract_url(str, items)
-  search_engines = items.filter { |item| item.type == :search_engine }
+	search_engines = items.filter { |item| item.type == :search_engine }
 
-  SearchItem.parse(str) do |url, st, se|
-    selected_se = search_engines.find { |s| s.title == se }
-    default_se = search_engines.find { |s| s.title == '*' }
+	SearchItem.parse(str) do |url, st, se|
+		selected_se = search_engines.find { |s| s.title == se }
+		default_se = search_engines.find { |s| s.title == '*' }
 
-    if str.start_with?('localhost')
-      "http://#{str}"
-    elsif SCHEMES.any? { |scheme| str.start_with? scheme }
-      str
-    elsif selected_se
-      selected_se.url.gsub('%s', URI.encode_uri_component(st))
-    elsif url
-      url
-    else
-      default_se.url.gsub('%s', URI.encode_uri_component(str))
-    end
-  end
+		if str.start_with?('localhost')
+			"http://#{str}"
+		elsif SCHEMES.any? { |scheme| str.start_with? scheme }
+			str
+		elsif selected_se
+			selected_se.url.gsub('%s', URI.encode_uri_component(st))
+		elsif url
+			url
+		else
+			default_se.url.gsub('%s', URI.encode_uri_component(str))
+		end
+	end
 end
 
 def main(opts)
-  browser = opts[:browser] || ENV['BROWSER']
+	browser = opts[:browser] || ENV['BROWSER']
 
-  items = Browser.all(
-    exclude_bookmarks: opts[:"exclude-bookmarks"],
-    exclude_history: opts[:"exclude-history"]
-  )
+	items = Browser.all(
+		exclude_bookmarks: opts[:"exclude-bookmarks"],
+		exclude_history: opts[:"exclude-history"]
+	)
 
-  selection = Open3.popen3(*LAUNCHER) do |stdin, stdout|
-    stdin.puts items.map(&:to_launcher_s)
-    stdin.close
-    begin
-      stdout.readline
-    rescue EOFError
-      exit 0
-    end
-  end
+	selection = Open3.popen3(*LAUNCHER) do |stdin, stdout|
+		stdin.puts items.map(&:to_launcher_s)
+		stdin.close
+		begin
+			stdout.readline
+		rescue EOFError
+			exit 0
+		end
+	end
 
-  url = extract_url(selection, items)
+	url = extract_url(selection, items)
 
-  exec "#{browser} #{url}"
+	exec "#{browser} #{url}"
 end
 
 def info
-  Browser.subclasses.each do |b|
-    puts <<~TEXT
-      #{b.name}
-      ---------
-      bookmarks: #{b.bookmarks.length}
-      history items: #{b.history.length}
+	Browser.subclasses.each do |b|
+		puts <<~TEXT
+			#{b.name}
+			---------
+			bookmarks: #{b.bookmarks.length}
+			history items: #{b.history.length}
 
-    TEXT
-  end
+		TEXT
+	end
 end
 
 if __FILE__ == $0
-  opts = {}
+	opts = {}
 
-  OptionParser.new do |p|
-    p.banner = 'Usage: omnibar [OPTIONS]'
-    p.on('--exclude-bookmarks', 'Exclude bookmarks')
-    p.on('--exclude-history', 'Exclude history')
-    p.on('--info', 'Print info')
-    p.on('--browser=BROWSER', 'Browser path')
-    p.parse!(into: opts)
-  end
+	OptionParser.new do |p|
+		p.banner = 'Usage: omnibar [OPTIONS]'
+		p.on('--exclude-bookmarks', 'Exclude bookmarks')
+		p.on('--exclude-history', 'Exclude history')
+		p.on('--info', 'Print info')
+		p.on('--browser=BROWSER', 'Browser path')
+		p.parse!(into: opts)
+	end
 
-  if opts[:info]
-    info
-    exit 0
-  end
+	if opts[:info]
+		info
+		exit 0
+	end
 
-  main(opts)
+	main(opts)
 end
--- omnibar_test.rb
+++ omnibar_test.rb
@@ -7,77 +7,77 @@ require 'test/unit'
 require_relative 'omnibar'
 
 class TestString < Test::Unit::TestCase
-  def test_lsplit
-    assert_equal 'a,b,c'.lsplit(','), ['a', 'b,c']
-  end
+	def test_lsplit
+		assert_equal 'a,b,c'.lsplit(','), ['a', 'b,c']
+	end
 
-  def test_rsplit
-    assert_equal 'a,b,c'.rsplit(','), ['a,b', 'c']
-  end
+	def test_rsplit
+		assert_equal 'a,b,c'.rsplit(','), ['a,b', 'c']
+	end
 end
 
 class TestParser < Test::Unit::TestCase
-  include Parser
+	include Parser
 
-  def test_whitespace?
-    assert_not_nil whitespace?('')
-    assert_nil whitespace?('hello')
-  end
+	def test_whitespace?
+		assert_not_nil whitespace?('')
+		assert_nil whitespace?('hello')
+	end
 
-  def test_comment?
-    assert_not_nil comment?('# hello')
-    assert_nil comment?('')
-    assert_nil comment?('hello')
-  end
+	def test_comment?
+		assert_not_nil comment?('# hello')
+		assert_nil comment?('')
+		assert_nil comment?('hello')
+	end
 end
 
 class TestSearchItem < Test::Unit::TestCase
-  def test_to_launcher_s
-    bookmark = SearchItem.new('example', 'https://example.com', nil, :bookmark)
-    history = SearchItem.new('example', 'https://example.com', '2024-10-14', :history)
-    search_engine = SearchItem.new('mdn', 'https://developer.mozilla.org/en-US/search?q=%s', nil, :search_engine)
+	def test_to_launcher_s
+		bookmark = SearchItem.new('example', 'https://example.com', nil, :bookmark)
+		history = SearchItem.new('example', 'https://example.com', '2024-10-14', :history)
+		search_engine = SearchItem.new('mdn', 'https://developer.mozilla.org/en-US/search?q=%s', nil, :search_engine)
 
-    assert_equal bookmark.to_launcher_s, 'B example <https://example.com>'
-    assert_equal history.to_launcher_s, 'H example <https://example.com> (2024-10-14)'
-    assert_equal search_engine.to_launcher_s, 'mdn: '
-  end
+		assert_equal bookmark.to_launcher_s, 'B example <https://example.com>'
+		assert_equal history.to_launcher_s, 'H example <https://example.com> (2024-10-14)'
+		assert_equal search_engine.to_launcher_s, 'mdn: '
+	end
 
-  def test_parse
-    SearchItem.parse('B example <https://example.com>') do |url, st, se|
-      assert_equal url, 'https://example.com'
-      assert_nil st
-      assert_nil se
-    end
+	def test_parse
+		SearchItem.parse('B example <https://example.com>') do |url, st, se|
+			assert_equal url, 'https://example.com'
+			assert_nil st
+			assert_nil se
+		end
 
-    SearchItem.parse('H example <https://example.com> (2024-10-14)') do |url, st, se|
-      assert_equal url, 'https://example.com'
-      assert_nil st
-      assert_nil se
-    end
+		SearchItem.parse('H example <https://example.com> (2024-10-14)') do |url, st, se|
+			assert_equal url, 'https://example.com'
+			assert_nil st
+			assert_nil se
+		end
 
-    SearchItem.parse('mdn: background-color') do |url, st, se|
-      assert_nil url
-      assert_equal st, 'background-color'
-      assert_equal se, 'mdn'
-    end
-  end
+		SearchItem.parse('mdn: background-color') do |url, st, se|
+			assert_nil url
+			assert_equal st, 'background-color'
+			assert_equal se, 'mdn'
+		end
+	end
 end
 
 class TestMain < Test::Unit::TestCase
-  def test_extract_url
-    search_engines = [
-      SearchItem.new('mdn', 'https://developer.mozilla.org/en-US/search?q=%s', nil, :search_engine),
-      SearchItem.new('clhs', 'http://www.xach.com/clhs?q=%s', nil, :search_engine),
-      SearchItem.new('*', 'https://html.duckduckgo.com/html?q=%s', nil, :search_engine),
-    ]
+	def test_extract_url
+		search_engines = [
+			SearchItem.new('mdn', 'https://developer.mozilla.org/en-US/search?q=%s', nil, :search_engine),
+			SearchItem.new('clhs', 'http://www.xach.com/clhs?q=%s', nil, :search_engine),
+			SearchItem.new('*', 'https://html.duckduckgo.com/html?q=%s', nil, :search_engine),
+		]
 
-    assert_equal extract_url('localhost:3000', search_engines), 'http://localhost:3000'
-    assert_equal extract_url('https://lebani.dev', search_engines), 'https://lebani.dev'
-    assert_equal extract_url('file:///home/aron/index.html', search_engines), 'file:///home/aron/index.html'
-    assert_equal extract_url('B example <https://example.com>', search_engines), 'https://example.com'
-    assert_equal extract_url('H example <https://example.com> (2024-10-14)', search_engines), 'https://example.com'
-    assert_equal extract_url('clhs: defun', search_engines), 'http://www.xach.com/clhs?q=defun'
-    assert_equal extract_url('mdn: hello world', search_engines), 'https://developer.mozilla.org/en-US/search?q=hello%20world'
-    assert_equal extract_url('hello world', search_engines), 'https://html.duckduckgo.com/html?q=hello%20world'
-  end
+		assert_equal extract_url('localhost:3000', search_engines), 'http://localhost:3000'
+		assert_equal extract_url('https://lebani.dev', search_engines), 'https://lebani.dev'
+		assert_equal extract_url('file:///home/aron/index.html', search_engines), 'file:///home/aron/index.html'
+		assert_equal extract_url('B example <https://example.com>', search_engines), 'https://example.com'
+		assert_equal extract_url('H example <https://example.com> (2024-10-14)', search_engines), 'https://example.com'
+		assert_equal extract_url('clhs: defun', search_engines), 'http://www.xach.com/clhs?q=defun'
+		assert_equal extract_url('mdn: hello world', search_engines), 'https://developer.mozilla.org/en-US/search?q=hello%20world'
+		assert_equal extract_url('hello world', search_engines), 'https://html.duckduckgo.com/html?q=hello%20world'
+	end
 end
